765e0d609230c2a8a2f6809e34529ef933572251
Abdelfetah Ouwahbi <ouahbiabdo99@gmail.com>
2024-05-10 11:30:39 +0100

    fixing up some issues in Main Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ import Main from './public_pages/Main_page'
 // SysAdmin Routes
 import SysDashboard from './users_pages/sysAdmin_pages/SysDashboard';
 import SysNotifications from './users_pages/sysAdmin_pages/SysNotifications';
-import SysAddConsultant from './users_pages/sysAdmin_pages/SysAddConsultant';
 import SysAllConsultants from './users_pages/sysAdmin_pages/SysAllConsultants';
-import SysAddOrganism from './users_pages/sysAdmin_pages/SysAddOrganism';
 import SysAllOrganismes from './users_pages/sysAdmin_pages/SysAllOrganismes';
 export default function App() {
   return (
