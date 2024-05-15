eaa3361e97e7fa671b7bd7bce30e2f466c597b2e
Abdelfetah Ouwahbi <ouahbiabdo99@gmail.com>
2024-05-15 12:58:40 +0100

    still working on the norm and children table

--- src/users_pages/client_pages/consultant_pages/AllDiagnosises.jsx
+++ src/users_pages/client_pages/consultant_pages/AllDiagnosises.jsx
@@ -1,16 +1,156 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaBars } from 'react-icons/fa';
+import Cookies from 'js-cookie';
+import { Modal, Button, Datepicker } from 'flowbite-react';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import toast, { Toaster } from 'react-hot-toast';
 import ClientMainPage from '../ClientMainPage';
 import { DiagnosisModal } from './DiagnosisModal';
+import { serverAddress } from '../../../ServerAddress';
+import { NewlyAddedDiagnosises } from './NewlyAddedDiagnosisDetails';
 
 export default function AllDiagnosises() {
 
     const [isClientMenuOpen, setIsClientMenuOpen] = useState(false);
     const [isDiagnosisModalOpen, setIsDiagnosisModalOpen] = useState(false);
+    //Table to fill with the norm, chapter and criterias
+    const [areDiagnosisDetailsShown, setAreDiagnosisDetailsShowen] = useState(false);
+    //Norm Id And Entreprise Id that will be sent to the Table 
+    const [chosenDiagnosisCode, setChosenDiagnosisCode] = useState('');
+    const [chosenNormeId, setChosenNormeId] = useState('');
+    const [chosenEntrepriseId, setChosenEntrepriseId] = useState('');
+
+    const [diagnosisId, setDiagnosisId] = useState([]);
+    const [diagnosisCode, setDiagnosisCode] = useState([]);
+    const [diagnosisDate, setDiagnosisDate] = useState([]);
+    const [diagnosisEntrepriseId, setDiagnosisEntrepriseId] = useState([]);
+    const [diagnosisEntrepriseName, setDiagnosisEntrepriseName] = useState([]);
+    const [diagnosisNormId, setDiagnosisNormId] = useState([]);
+    const [diagnosisNormName, setDiagnosisNormName] = useState([]);
+    const [isdiagnosisDone, setIsDiagnosisDone] = useState([]);
+
+    //DELETE VARIABLES
+    const [isDeletingDiagnosis, setIsDeletingDiagnosis] = useState(false);
+    const [diagnosisToDelete, setDiagnosisToDelete] = useState(0);
+
+    //UPDATE VARIABLES
+    //Will take the id of the diagnosis and from there we'll toogle the visibility of the DPicker by camparing it with the current Diag Id 
+    const [isChangingDate, setIsChangingDate] = useState(0);
+    const [dateToChange, setDateToChange] = useState(new Date());
+
+    const getAllDiagnosises = async () => {
+        setDiagnosisId([]);
+        setDiagnosisCode([]);
+        setDiagnosisDate([]);
+        setDiagnosisEntrepriseId([]);
+        setDiagnosisEntrepriseName([]);
+        setDiagnosisNormId([]);
+        setDiagnosisNormName([]);
+        setIsDiagnosisDone([]);
+        try {
+            const response = await fetch(`http://${serverAddress}:8080/api/v1/diagnosis`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${Cookies.get("JWT")}`
+                }
+            });
+            const data = await response.json();
+            console.log("Got all diagnosises --> ", data);
+            setDiagnosisId(data.map(d => d.id));
+            setDiagnosisCode(data.map(d => d.code));
+            setDiagnosisDate(data.map(d => d.date));
+            setDiagnosisEntrepriseId(data.map(d => d.entrepriseId));
+            setDiagnosisEntrepriseName(data.map(d => d.entrepriseRaisonSociale));
+            setDiagnosisNormId(data.map(d => d.normeId));
+            setDiagnosisNormName(data.map(d => d.normeLabel));
+            setIsDiagnosisDone(data.map(d => d.isDone));
+        } catch (error) {
+            console.error("error getting all diagnosises -> ", error)
+        }
+    }
+
+    //On Mount We'll get all the saved Diagnosises
+    useEffect(() => {
+        if (diagnosisId.length > 0) {
+            console.log("Already got all Diagnosises !")
+        } else {
+            getAllDiagnosises();
+        }
+    }, [])
+
+    //Function that updates a diagnosis
+    const updateDiagnosis = async (diagnosis, dateToChange) => {
+        if (dateToChange.getTime() < new Date().getTime()) {
+            console.log("You can't change to a date in the past !!");
+            toast.error("vous devez choisir une date supérieur al la date d'aujourd'hui !!")
+        } else {
+            try {
+                const response = await fetch(`http://${serverAddress}:8080/api/v1/diagnosis/${diagnosis}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${Cookies.get("JWT")}`,
+                    },
+                    body: JSON.stringify({
+                        "date": dateToChange,
+                    }),
+                });
+                if (response.ok) {
+                    console.log("Diagnosis updated successfully");
+                    toast.success("La date de ce diagnostic a été modifiée avec succès ..", {
+                        duration: 2000
+                    });
+
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 2000);
+
+                } else {
+                    console.error("Failed to update diagnosis. Status:", response.status);
+                    toast.error("une errur s'est produit, la date n'a été modifiée !!")
+                }
+            } catch (error) {
+                console.error("error updating diagnosis --> ", error)
+            }
+
+            setIsChangingDate(0);
+        }
+    }
+
+    //Function that deletes a diagnosis
+    const destroyDiagnosis = async (diagnosis) => {
+        try {
+            const response = await fetch(`http://${serverAddress}:8080/api/v1/diagnosis/${diagnosis}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${Cookies.get("JWT")}`,
+                    'Content-Type': 'application/json',
+                }
+            });
+
+            if (response.ok) {
+                setIsDeletingDiagnosis(false);
+                getAllDiagnosises();
+                console.log("Diagnosis Destroyed successfully ..");
+                toast.success("Ce diagnostic est éliminé ..")
+            } else {
+                throw new Error(`Failed to delete diagnosis: ${response.status} ${response.statusText}`);
+            }
+        } catch (error) {
+            console.error('Error deleting diagnosis:', error);
+            // Handle error
+            throw error; // Optionally re-throw the error for the caller to handle
+        }
+    }
+
 
     return (
         <>
-            <div className="flex flex-col w-full h-auto">
+            <Toaster
+                position="top-center"
+                reverseOrder={false}
+            />
+            <div className="flex flex-col w-full h-auto pb-10">
                 <div className="flex p-4 w-full items-center justify-between">
                     {/* Bars Icon That toogles the visibility of the menu */}
                     <FaBars onClick={() => setIsClientMenuOpen(!isClientMenuOpen)} className='w-6 h-6 cursor-pointer text-neutral-600' />
@@ -30,8 +170,118 @@ export default function AllDiagnosises() {
                 </div>
 
             </div>
+
+            {/* Table des diagnostics */}
+
+            <div className="px-2 md:px-8 shadow-md">
+                <table id="consultantsTable"
+                    className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                Code
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Date de diagnostic
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Entreprise Cliente
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Norme choisi
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Actions
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {diagnosisId.map((dgns, index) =>
+                            <tr key={index} className="border-b">
+                                <td className="px-6 py-4">{diagnosisCode[index]}</td>
+                                {isChangingDate === diagnosisId[index] ? (
+
+                                    <Datepicker
+                                        value={dateToChange}
+                                        language="FR-fr"
+                                        onSelectedDateChanged={(date) => setDateToChange(date)}
+                                        className='z-0'
+                                    />
+
+                                ) : (
+                                    <td className="px-6 py-4">{diagnosisDate[index].replace('T', ' a ')}</td>
+                                )}
+                                <td className="px-6 py-4">{diagnosisEntrepriseName[index]}</td>
+                                <td className="px-6 py-4">{diagnosisNormName[index]}</td>
+                                <td className="px-6 py-4">
+                                    <div className="flex items-center gap-4">
+                                        {isdiagnosisDone[index] === false &&
+                                            <>
+                                                {isChangingDate === diagnosisId[index] ?
+                                                    <>
+                                                        <button onClick={() => updateDiagnosis(diagnosisId[index], dateToChange)} className="font-medium text-green-500 hover:underline">Enregistrer</button>
+                                                        <button onClick={() => setIsChangingDate(false)} className="font-medium text-red-600 hover:underline">Annuler</button>
+                                                    </>
+                                                    :
+                                                    <>
+                                                        <button onClick={() => {
+                                                            setChosenDiagnosisCode(diagnosisCode[index]);
+                                                            setChosenEntrepriseId(diagnosisEntrepriseId[index]);
+                                                            setChosenNormeId(diagnosisNormId[index]);
+                                                            setAreDiagnosisDetailsShowen(true);
+                                                        }} className="font-medium text-blue-600 hover:underline">Commencer</button>
+                                                        <button onClick={() => {
+                                                            setIsChangingDate(diagnosisId[index]);
+                                                        }} className="font-medium text-green-500 hover:underline">Changer La date</button>
+                                                        <button onClick={() => {
+                                                            setIsDeletingDiagnosis(true);
+                                                            setDiagnosisToDelete(diagnosisId[index]);
+                                                        }} className="font-medium text-red-600 hover:underline">Detruire</button>
+                                                    </>
+                                                }
+
+                                            </>
+                                        }
+                                        {isdiagnosisDone[index] === true &&
+                                            <h1 className='font-p_black text-gray-500 opacity-70'>(Deja fait le {diagnosisDate[index].replace('T', ' a ')})</h1>
+                                        }
+                                    </div>
+                                </td>
+                            </tr>
+                        )}
+
+                    </tbody>
+                </table>
+            </div>
+
+
             {isClientMenuOpen && <ClientMainPage onClose={() => setIsClientMenuOpen(false)} />}
-            {isDiagnosisModalOpen && <DiagnosisModal onClose={() => setIsDiagnosisModalOpen(false)}/>}
+            {isDiagnosisModalOpen && <DiagnosisModal onClose={() => setIsDiagnosisModalOpen(false)} />}
+            {areDiagnosisDetailsShown && <NewlyAddedDiagnosises DiagnosisCode={chosenDiagnosisCode} chosenNormeId={chosenNormeId} chosenEntrepriseId={chosenEntrepriseId} onClose={() => setAreDiagnosisDetailsShowen(false)} />}
+            <Modal show={isDeletingDiagnosis} size="md" onClose={() => setIsDeletingDiagnosis(false)}
+                popup>
+                <Modal.Header />
+                <Modal.Body>
+                    <div className="text-center">
+                        <HiOutlineExclamationCircle
+                            className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                        <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                            Êtes-vous sûr que vous voulez detruire ce diagnostic ?
+                        </h3>
+                        <h3 className='mb-5 text-lg font-normal text-sky-700'>notez bien que vous pouvez modifier la date !!</h3>
+                        <div className="flex justify-center gap-4">
+                            <Button color="failure" onClick={() => {
+                                destroyDiagnosis(diagnosisToDelete);
+                            }}>
+                                {"Oui, je suis sur"}
+                            </Button>
+                            <Button color="gray" onClick={() => setIsDeletingDiagnosis(false)}>
+                                Non, Annuler
+                            </Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
         </>
     )
 }
--- src/users_pages/client_pages/consultant_pages/NewlyAddedDiagnosisDetails.jsx
+++ src/users_pages/client_pages/consultant_pages/NewlyAddedDiagnosisDetails.jsx
@@ -1,29 +1,108 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Modal } from "flowbite-react";
+import Cookies from "js-cookie";
+import { serverAddress } from "../../../ServerAddress";
 
-export function NewlyAddedDiagnosises({chosenEntrepriseId, chosenNormeId, onClose}) {
+export function NewlyAddedDiagnosises({DiagnosisCode, chosenEntrepriseId, chosenNormeId, onClose }) {
 
+  console.log("received Diagnosis Code -->", DiagnosisCode);
   console.log("received Entreprise ID -->", chosenEntrepriseId);
   console.log("received Norm ID -->", chosenNormeId);
 
+
   const [openModal, setOpenModal] = useState(true);
+
+  const [normAndChildren, setNormAndChildren] = useState({});
+
+  //Function that gets the norm and the chapters and the criterias
+  useEffect(() => {
+    if (Object.keys(normAndChildren).length > 0) {
+      console.log("Already get the whole Table");
+    } else {
+      const getNormAndChildren = async () => {
+        try {
+          const response = await fetch(`http://${serverAddress}:8080/api/v1/normes/${chosenNormeId}`, {
+            method: 'GET',
+            headers: {
+              'Authorization': `Bearer ${Cookies.get("JWT")}`,
+            }
+          });
+  
+          const data = await response.json();
+          console.log("Got the chosen norm and its children -->",data);
+          setNormAndChildren(data)
+        } catch (error) {
+          console.log("error happened while getting the norm and its children -->", error)
+        }
+      }
+      getNormAndChildren();
+    }
+  }, [])
 
   return (
     <>
-      <Modal show={openModal} onClose={onClose}>
-        <Modal.Header>Terms of Service</Modal.Header>
-        <Modal.Body>
-          <div className="space-y-6">
-            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
-              companies around the world are updating their terms of service agreements to comply.
-            </p>
-            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant
-              to ensure a common set of data rights in the European Union. It requires organizations to notify users as
-              soon as possible of high-risk data breaches that could personally affect them.
-            </p>
+      <Modal show={openModal} size='xxl' onClose={onClose}>
+        <Modal.Header>
+          <div className='flex flex-col gap-4'>
+            <h1>Code de Diagnostic :  </h1>
+            <h1>Entreprise Cliente :  {chosenEntrepriseId}</h1>
+            <h1>La Norme Choisi :  {chosenNormeId}</h1>
           </div>
+        </Modal.Header>
+        <Modal.Body>
+
+          <table id="consultantsTable"
+            className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">Norme</th>
+                <th scope="col" className="px-6 py-3">Chapitre</th>
+                <th scope="col" className="px-6 py-3">Critère</th>
+              </tr>
+            </thead>
+
+            <tbody>
+
+              <tr className="border-b">
+                <td className="px-6 py-4" rowSpan="9">Category 1</td> {/* rowSpan'll be the addition of lengths of chapter & criteria*/}
+                <td className="px-6 py-4" rowSpan="3">Subcategory 1.1</td> {/* rowSpan'll be the length of criteria array*/}
+                <td className="px-6 py-4">Critère 1.1.1</td>
+              </tr>
+
+              <tr className="border-b">
+                <td className="px-6 py-4">Critère 1.1.2</td>
+              </tr>
+              <tr className="border-b">
+                <td className="px-6 py-4">Critère 1.1.3</td>
+              </tr>
+
+              <tr className="border-b">
+                <td className="px-6 py-4" rowSpan="3">Subcategory 1.2</td>
+                <td className="px-6 py-4">Critère 1.2.1</td>
+              </tr>
+
+              <tr className="border-b">
+                <td className="px-6 py-4">Critère 1.2.2</td>
+              </tr>
+              <tr className="border-b">
+                <td className="px-6 py-4">Critère 1.2.3</td>
+              </tr>
+              <tr className="border-b">
+                <td className="px-6 py-4" rowSpan="3">Subcategory 1.3</td>
+                <td className="px-6 py-4">Critère 1.3.1</td>
+              </tr>
+              <tr className="border-b">
+                <td className="px-6 py-4">Critère 1.3.2</td>
+              </tr>
+              <tr className="border-b">
+                <td className="px-6 py-4">Critère 1.3.3</td>
+              </tr>
+
+            </tbody>
+          </table>
+
+
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={() => setOpenModal(false)}>I accept</Button>
